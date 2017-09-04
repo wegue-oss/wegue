27d10daaff93eb35f0e49ee32de830d7f4660cc8
Christian Mayer <chris@meggsimum.de>
2017-09-04 16:30:58 +0200

    Code cleanup in map component

--- src/components/ol/OlMap.vue
+++ src/components/ol/OlMap.vue
@@ -2,19 +2,14 @@
     <div class="map" id="map">
 
       <slot name="single-layer">LAYER SLOT DEFAULT / FALLBACK</slot>
-      <!-- <ol-layer-osm /> -->
 
     </div>
 </template>
 
 <script>
 
-import OsmLayer2 from './OlLayerOsm.vue'
-
 import Map from 'ol/map'
 import View from 'ol/view'
-// import TileLayer from 'ol/layer/tile'
-// import XYZ from 'ol/source/xyz'
 
 export default {
   name: 'ol-map',
@@ -23,14 +18,10 @@ export default {
     }
   },
   props: ['zoom'],
-  components: {
-    'ol-layer-osm': OsmLayer2
-  },
   mounted () {
     this.map.setTarget(document.getElementById('map'))
   },
   created () {
-    console.log('CRETAED MAP', this)
     this.map = new Map({
       target: 'map',
       layers: [
