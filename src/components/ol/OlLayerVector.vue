254b35df5f4834848d540fbc82afaed00ebbc491
Christian Mayer <chris@meggsimum.de>
2017-11-10 14:48:43 +0100

    Make vector layers selectable
    This introduces a mechanism to make vector layers selectbale via config
    property. Registers a select interaction for the layer and fires a global event
    'map-selectionchange' (via EventBus).

--- src/components/ol/OlLayerVector.vue
+++ src/components/ol/OlLayerVector.vue
@@ -5,17 +5,49 @@ import VectorSource from 'ol/source/vector'
 import KmlFormat from 'ol/format/kml'
 import GeoJsonFormat from 'ol/format/geojson'
 import OlLayerBase from './OlLayerBase'
+import SelectInteraction from 'ol/interaction/select'
+import { EventBus } from '../../EventBus.js'
 
 var OlLayerVector = OlLayerBase.extend({
   name: 'ol-layer-vector',
   props: {
     url: {type: String, required: true},
     format: {type: String, required: true},
-    formatConfig: {type: Object, required: false, default: {}}
+    formatConfig: {type: Object, required: false, default: {}},
+    selectable: {type: Boolean, required: false, default: false}
+  },
+  created: function () {
+    var me = this
+    // listen for the ol-map-mounted event and receive the OL map
+    // instance
+    EventBus.$on('ol-map-mounted', function (olMap) {
+      // make the OL map accesible in this component
+      me.map = olMap;
+
+      // if layer is selectable register a select interaction
+      if (me.selectable) {
+        var selectClick = new SelectInteraction({
+          layers: [me.layer]
+        });
+
+        // forward an event if feature selection changes
+        selectClick.on('select', function (evt) {
+          // TODO use identifier for layer (once its implemented)
+          EventBus.$emit(
+            'map-selectionchange',
+            me.layer.get('name'),
+            evt.selected,
+            evt.deselected
+          );
+        });
+        // register/activate interaction on map
+        me.map.addInteraction(selectClick);
+      }
+    });
   },
   methods: {
     createLayer () {
-      // map the formats to short keys
+      // map the supported formats to short keys
       const formatMapping = {
         'KML': KmlFormat,
         'GeoJSON': GeoJsonFormat
@@ -27,7 +59,7 @@ var OlLayerVector = OlLayerBase.extend({
           url: this.url,
           format: new formatMapping[this.format](this.formatConfig)
         })
-      })
+      });
 
       return vectorLayer
     }
