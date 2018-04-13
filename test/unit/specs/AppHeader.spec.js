21b3c32fd3fd39ef4f50efab4b0fdeef439d2de4
Christian Mayer <chris@meggsimum.de>
2018-04-13 17:07:57 +0200

    Fix unit tests for AppHeader cmp

--- test/unit/specs/AppHeader.spec.js
+++ test/unit/specs/AppHeader.spec.js
@@ -2,14 +2,13 @@ import Vue from 'vue'
 import AppHeader from '@/components/AppHeader'
 
 describe('AppHeader.vue', () => {
-  // Inspect the raw component options
-  it('has a props object', () => {
-    expect(typeof AppHeader.props).to.equal('object');
-  });
 
   // Evaluate the results of functions in
   // the raw component options
   it('sets the correct default data', () => {
+    AppHeader.$appConfig = {
+      title: 'foo'
+    };
     expect(typeof AppHeader.data).to.equal('function');
     const defaultData = AppHeader.data();
     expect(typeof defaultData).to.equal('object');
@@ -17,10 +16,14 @@ describe('AppHeader.vue', () => {
 
   // Mount an instance and inspect the render output
   it('renders the correct title', () => {
-    const Constructor = Vue.extend(AppHeader)
-    const vm = new Constructor({
+    AppHeader.$appConfig = {
       title: 'foo'
+    };
+    const Constructor = Vue.extend(AppHeader);
+    const vm = new Constructor({
+      title: 'foo',
     }).$mount();
+
     expect(vm.$el.querySelector('v-toolbar-title') !== null).to.equal(true);
   });
 });
