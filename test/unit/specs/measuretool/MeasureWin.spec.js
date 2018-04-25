70e8d3500a9ec3ebbc066637ec12b791d3ec7cf7
Christian Mayer <chris@meggsimum.de>
2018-04-25 10:20:10 +0200

    Cleanup unit tests due to Mapable mixin introduction

--- test/unit/specs/measuretool/MeasureWin.spec.js
+++ test/unit/specs/measuretool/MeasureWin.spec.js
@@ -2,11 +2,6 @@ import Vue from 'vue'
 import MeasureWin from '@/components/measuretool/MeasureWin'
 
 describe('measuretool/MeasureWin.vue', () => {
-  // Inspect the raw component options
-  it('has a created hook', () => {
-    expect(typeof MeasureWin.created).to.equal('function');
-  });
-
   // Evaluate the results of functions in
   // the raw component options
   it('sets the correct default data', () => {
