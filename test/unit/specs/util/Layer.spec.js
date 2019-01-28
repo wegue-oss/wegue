c5e755b9d0a0740535fca91fc82d30385780ddf4
Christian Mayer <chris@meggsimum.de>
2019-01-28 12:27:36 +0100

    Add unit tests for LayerUtil

---
+++ test/unit/specs/util/Layer.spec.js
@@ -0,0 +1,112 @@
+import LayerUtil from '@/util/Layer'
+import Map from 'ol/Map'
+import View from 'ol/View'
+import TileLayer from 'ol/layer/Tile'
+import OSM from 'ol/source/OSM'
+
+describe('LayerUtil', () => {
+  it('is defined', () => {
+    expect(typeof UrlUtil).to.not.equal(undefined);
+  });
+
+  it('has the correct functions', () => {
+    expect(typeof LayerUtil.getLayersBy).to.equal('function');
+    expect(typeof LayerUtil.getLayerByLid).to.equal('function');
+  });
+
+  it('getLayersBy returns correct layers wrapped as array', () => {
+    const olMap = new Map({
+      layers: [
+        new TileLayer({
+          foo: 'bar',
+          source: new OSM()
+        }),
+        new TileLayer({
+          foo: 'bar',
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+    const layerArr = LayerUtil.getLayersBy('foo', 'bar', olMap);
+    expect(layerArr).to.be.an('array');
+    expect(layerArr.length).to.eql(2);
+  });
+
+  it('getLayersBy returns empty array for non matching input params', () => {
+    const olMap = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+    const layerArr = LayerUtil.getLayersBy('foo', 'bar', olMap);
+    expect(layerArr).to.be.an('array');
+    expect(layerArr).to.eql([]);
+  });
+
+  it('getLayersBy returns empty array if no OL map is passed', () => {
+    const layerArr = LayerUtil.getLayersBy('foo', 'bar');
+    expect(layerArr).to.be.an('array');
+    expect(layerArr).to.eql([]);
+  });
+
+  it('getLayerByLid returns correct layer', () => {
+    const olMap = new Map({
+      layers: [
+        new TileLayer({
+          lid: 'bar',
+          name: 'Test',
+          source: new OSM()
+        }),
+        new TileLayer({
+          foo2: 'bar',
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+    const layer = LayerUtil.getLayerByLid('bar', olMap);
+    expect(typeof layer).to.not.equal(undefined);
+    expect(layer).to.be.an('object');
+    expect(layer.get('name')).to.eql('Test')
+  });
+
+  it('getLayerByLid returns undefined for non existing layer', () => {
+    const olMap = new Map({
+      layers: [
+        new TileLayer({
+          lid: 'bar',
+          name: 'Test',
+          source: new OSM()
+        }),
+        new TileLayer({
+          foo2: 'bar',
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+    const layer = LayerUtil.getLayerByLid('kalle', olMap);
+    expect(layer).to.equal(undefined);
+  });
+
+  it('getLayerByLid returns undefined if no OL map is passed', () => {
+    const layer = LayerUtil.getLayerByLid('kalle');
+    expect(layer).to.equal(undefined);
+  });
+});
