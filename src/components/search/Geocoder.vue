d3c3cb6f36c35d940de91fd8db23a40c3e4d3e13
Just van den Broecke <just@justobjects.nl>
2019-07-31 17:52:48 +0200

    meggsimum/wegue#29 first very rough working version with OSM Nominatim Provider

--- src/components/search/Geocoder.vue
+++ src/components/search/Geocoder.vue
@@ -10,13 +10,13 @@
       :dark="dark"
       :persistent-hint="persistentHint"
       :hidden="showSearch"
+      :rounded="rounded"
       :search-input.sync="search"
     ></v-combobox>
     
     <v-btn @click='toggle()' icon :dark="dark" slot="activator">
-      <v-icon medium>{{icon}}</v-icon>
-        {{text}}
-      </v-btn>
+      <v-icon medium>{{buttonIcon}}</v-icon>
+    </v-btn>
   </v-toolbar-items>
 
 </template>
@@ -24,56 +24,75 @@
 <script>
   import {Mapable} from '../../mixins/Mapable';
   import {PROVIDERS, Nominatim} from './nominatim';
+  import {applyTransform} from 'ol/extent';
+  import {getTransform} from 'ol/proj';
 
   export default {
     name: 'wgu-geocoder-input',
     mixins: [Mapable],
     props: {
-      color: {type: String, required: false, default: 'red darken-3'},
-      icon: {type: String, required: false, default: 'search'},
-      text: {type: String, required: false},
+      buttonIcon: {type: String, required: false, default: 'search'},
       label: {type: String, required: false, default: 'Nominatim Search'},
+      rounded: {type: Boolean, required: false, default: true},
       autofocus: {type: Boolean, required: false, default: true},
       dark: {type: Boolean, required: false, default: false},
       persistentHint: {type: Boolean, required: false, default: true},
       provider: {type: String, required: false, default: PROVIDERS.OSM},
-      minChars: {type: Number, required: false, default: 4},
-      gueryDelayMillis: {type: Number, required: false, default: 3000}
+      minChars: {type: Number, required: false, default: 5},
+      queryDelayMillis: {type: Number, required: false, default: 300},
+      debug: {type: Boolean, required: false, default: false}
     },
     data () {
       return {
         items: [],
+        lastSearch: '',
         search: '',
-        selected: '',
-        showSearch: true
-      }
-    },
-    computed: {
-      searchResults () {
-        console.info('searchResults computed:', this.selected);
-        return []
+        selected: null,
+        showSearch: true,
+        timeout: null
       }
     },
     methods: {
       toggle () {
         this.showSearch = !this.showSearch
       },
-      querySelections (val) {
-        console.info('querySelections:', val);
-        setTimeout(() => {
-          this.items = this.geocoder.query(val);
-        }, this.gueryDelayMillis);
-      },
-      onEnd () {
-        // When all done center map, set marker etc
-        const center = 'something';
-        this.map.getView().setCenter(center);
+      querySelections (str) {
+        this.debug && console.info('querySelections:', str);
+        this.timeout && clearTimeout(this.timeout);
+        this.timeout = setTimeout(() => {
+          // Let Geocoder Provider do the query
+          // items (item.text fields) will be shown in suggestions
+          this.debug && console.info('issueQuery:', str);
+          this.items = this.geocoder.query(str);
+        }, this.queryDelayMillis);
       }
     },
     watch: {
-      search (val) {
-        console.log(`search=${val}`);
-        val && val !== this.selected && val.length >= this.minChars && this.querySelections(val);
+      // Input string value changed
+      search (str) {
+        if (!str || str.length === 0) {
+          this.items = [];
+          return;
+        }
+
+        this.debug && console.info('search:', str);
+        // Only query when input changed from last with minimum number of chars
+        str && str !== this.lastSearch && str.length >= this.minChars && this.querySelections(str);
+        this.lastSearch = str;
+      },
+      // User has selected entry from suggested items
+      selected (item) {
+        if (!item.hasOwnProperty('text')) {
+          return;
+        }
+
+        this.debug && console.log(`selected=${item.text}`);
+        // bbox is in EPSG:4326, needs to be transformed to Map Projection (e.g. EPSG:3758)
+        // TODO: not all Providers return a bbox, need view.setCenter()/.setZoom() then
+        const bbox = item.value.boundingbox.map(x => Number.parseFloat(x));
+        let extent = [bbox[2], bbox[0], bbox[3], bbox[1]];
+        extent = applyTransform(extent, getTransform('EPSG:4326', this.map.getView().getProjection()));
+        this.map.getView().fit(extent);
       }
     },
     mounted () {
--- src/components/search/nominatim.js
+++ src/components/search/nominatim.js
@@ -1,3 +1,26 @@
+// Code modified from: https://github.com/jonataswalker/ol-geocoder version: July 30, 2019
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Jonatas Walker
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 // import LayerVector from 'ol/layer/Vector';
 // import SourceVector from 'ol/source/Vector';
 // import Point from 'ol/geom/Point';
@@ -107,10 +130,10 @@ export class Nominatim {
     response.forEach(row => {
       switch (this.options.provider) {
         case PROVIDERS.OSM:
-          this.result.push(row.address.name);
+          this.result.push({text: row.address.name, value: row});
           break;
         default:
-          this.result.push(row.address.name);
+          this.result.push({text: row.address.name, value: row});
       }
     });
     this.lastResult = this.result;
