4c11b0563719f2b3dc29bbaf1ecc15e720910810
Just van den Broecke <just@justobjects.nl>
2020-04-09 17:57:58 +0200

    #122 add Share Button: Permalink Sharing with email

--- src/components/sharebutton/ShareButton.vue
+++ src/components/sharebutton/ShareButton.vue
@@ -14,12 +14,29 @@
           </v-tooltip>
         </template>
         <v-list>
-          <v-list-tile
-            v-for="(item, index) in items"
-            :key="index"
-            @click="onClick(item.action)"
-          >
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          <v-list-tile @click="onCopyUrl()">
+            <v-list-tile-action>
+              <v-icon medium>file_copy</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{copyUrlText}}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile @click="onCopyEmbedHTML()">
+            <v-list-tile-action>
+               <v-icon medium>code</v-icon>
+             </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{copyEmbedHtmlText}}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-tile-action>
+               <v-icon medium>email</v-icon>
+             </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title v-html="mailToUrl"/>
+            </v-list-tile-content>
           </v-list-tile>
         </v-list>
       </v-menu>
@@ -33,29 +50,12 @@
         {{alertText}}
       </v-alert>
     </div>
-<!--  <div>-->
-<!--    <div>-->
-<!--    <v-btn icon :dark="dark" @click="onClick">-->
-<!--      <v-icon medium>{{icon}}</v-icon>-->
-<!--      {{text}}-->
-<!--    </v-btn>-->
-<!--    <textarea id="textstore" ref="textstore" style="display: none; position: relative"></textarea>-->
-<!--    </div>-->
-<!--    <v-alert-->
-<!--      class="wgu-alert"-->
-<!--        :value="alert"-->
-<!--        :dark="dark"-->
-<!--        type="info"-->
-<!--        transition="fade-transition"-->
-<!--      >-->
-<!--      {{alertText}}-->
-<!--    </v-alert>-->
-<!--  </div>-->
 </template>
 
 <script>
 
 import { Mapable } from '../../mixins/Mapable';
+import {WguEventBus} from '../../WguEventBus';
 
 export default {
   name: 'wgu-share-btn',
@@ -64,61 +64,95 @@ export default {
     icon: {type: String, required: false, default: 'share'},
     text: {type: String, required: false, default: ''},
     hoverText: {type: String, required: false, default: 'Share permalink'},
-    alertText: {type: String, required: false, default: 'Copied'},
     alertTimeMillis: {type: Number, required: false, default: 1000},
-    dark: {type: Boolean, required: false, default: false}
+    alertCopiedText: {type: String, required: false, default: 'Copied'},
+    alertErrorText: {type: String, required: false, default: 'An error occurred'},
+    dark: {type: Boolean, required: false, default: false},
+    copyUrlText: {type: String, required: false, default: 'Copy URL'},
+    copyEmbedHtmlText: {type: String, required: false, default: 'Copy HTML Embed Code'},
+    emailUrlText: {type: String, required: false, default: 'Email URL'},
+    emailSubjectText: {type: String, required: false, default: 'NPLH Url Share'}
   },
   data () {
     return {
       alert: false,
-      items: [
-        { title: 'Copy URL', action: 'copy' },
-        { title: 'Copy HTML Embed Code', action: 'embed' }
-        // { title: 'Email URL', action: 'email' }
-      ]
+      alertText: this.alertCopiedText,
+      mailToUrl: this.emailUrlText
     }
   },
+  mounted () {
+    // Listen to the ol-map-mounted event and receive the OL map instance
+    WguEventBus.$on('ol-map-mounted', (olMap) => {
+      // Listen to map state changes (pan, zoom)
+      this.map.on('moveend', () => {
+        this.onMapChange();
+      });
+    });
+  },
   methods: {
-    onClick (action) {
+    showAlert (text) {
+      // Show short alert
+      this.alertText = text || this.alertCopiedText;
+      this.alert = true;
+      window.setTimeout(() => {
+        this.alert = false;
+      }, this.alertTimeMillis);
+    },
+    copyToClipboard (text) {
+      const textArea = this.$refs.textstore;
+      // Show and select  textarea
+      textArea.style.display = 'block';
+      textArea.value = text;
+      textArea.select();
+
+      // Copy to clipboard
+      document.execCommand('copy');
+
+      // Hide textarea
+      textArea.value = '';
+      textArea.style.display = 'none';
+    },
+    onCopyUrl () {
       // Get permalink URL and copy to clipboard
-      const permalinkController = this.map.get('permalinkcontroller');
-      if (!permalinkController) {
-        console.error('no permalink controller available!');
-        return true;
-      }
       try {
         // Copy to clipboard: selected text needs to be visible so
         // toggle textarea short time
-        let text;
-
-        if (action === 'copy') {
-          text = permalinkController.getShareUrl();
-        } else if (action === 'embed') {
-          text = permalinkController.getEmbedHTML();
-        } else {
-          console.error('no permalink controller available!');
-          return true;
-        }
-        const textArea = this.$refs.textstore;
-        // Show and select  textarea
-        textArea.style.display = 'block';
-        textArea.value = text;
-        textArea.select();
-
-        // Copy to clipboard
-        document.execCommand('copy');
-        // Hide textarea
-        textArea.value = '';
-        textArea.style.display = 'none';
-
-        // Show short alert
-        this.alert = true;
-        window.setTimeout(() => {
-          this.alert = false;
-        }, this.alertTimeMillis);
+        const permalinkController = this.map.get('permalinkcontroller');
+        this.copyToClipboard(permalinkController.getShareUrl());
+        this.showAlert();
       } catch (error) {
-        console.error(error);
+        this.showAlert(this.alertErrorText);
       }
+    },
+    onCopyEmbedHTML () {
+      // Get permalink URL and copy to clipboard
+      try {
+        const permalinkController = this.map.get('permalinkcontroller');
+        this.copyToClipboard(permalinkController.getEmbedHTML());
+        this.showAlert();
+      } catch (error) {
+        this.showAlert(this.alertErrorText);
+      }
+    },
+    setMailtoUrl: function () {
+      // if (!this.map) {
+      //   return 'Waiting for Map...';
+      // }
+      // https://css-tricks.com/snippets/html/mailto-links/
+      // Get permalink URL and add to email Body
+      try {
+        const permalinkController = this.map.get('permalinkcontroller');
+        const url = encodeURIComponent(permalinkController.getShareUrl());
+        this.mailToUrl = `<a href="mailto:?subject=${this.emailSubjectText}&body=${url}">${this.emailUrlText}</a>`
+      } catch (error) {
+        this.showAlert(this.alertErrorText);
+      }
+    },
+    /**
+     * Callback when Map View has changed, e.g. 'moveend'.
+     */
+    onMapChange () {
+      this.setMailtoUrl();
     }
   }
 }
@@ -133,5 +167,8 @@ export default {
     right: 4em;
     z-index: 1;
   }
-
+  a {
+    color: inherit;
+    text-decoration: none !important;
+  }
 </style>
