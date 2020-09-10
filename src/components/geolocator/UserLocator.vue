5df1bf10e1be240f622f8df5360a34167140a71b
Jürgen Hahn <juergen.hahn@gisolutions.at>
2020-09-10 08:17:16 +0200

    final reworks finished changes of variable names and function names

--- src/components/geolocator/UserLocator.vue
+++ src/components/geolocator/UserLocator.vue
@@ -1,9 +1,9 @@
 <template>
 <span>
-   <v-btn @click="locateMePressed" icon dark>
-      <v-icon v-if='this.isSearchingForPosition'>update</v-icon>
-      <v-icon v-else-if='this.isGPSAvailable && (!this.isPositionFixed)'>location_searching</v-icon>
-      <v-icon v-else-if='this.isGPSAvailable && this.isPositionFixed'>gps_fixed</v-icon>
+   <v-btn @click="geolocateUserAndShowMarkerOnMap" icon dark>
+      <v-icon v-if='this.isSearchingForGeolocation'>update</v-icon>
+      <v-icon v-else-if='this.isGeolocationAPIAvailable && (!this.isGeolocationFound)'>location_searching</v-icon>
+      <v-icon v-else-if='this.isGeolocationAPIAvailable && this.isGeolocationFound'>gps_fixed</v-icon>
       <v-icon v-else>location_disabled</v-icon>
     </v-btn>
 </span>
@@ -19,37 +19,39 @@ import { Fill, Style, Text } from 'ol/style';
 
 import { WguEventBus } from '../../WguEventBus'
 
-function getGeolocationLayer (layers) {
-  var geolocationLid = 'userPosition';
+// definition for the geolocation marker of the device on the map
+const geolocationMarker = new Style({
+  text: new Text({
+    text: 'person_pin_circle',
+    font: 'normal 30px Material Icons',
+    fill: new Fill({
+      color: 'blue'
+    })
+  })
+});
+
+function createAndRemoveExistingLayer (layers, layerId) {
   // if the geolocationLayer is already included it has to be removed
-  layers.remove(layers.getArray().filter(layer => geolocationLid === (layer.get('lid')))[0]);
-  let layer = new VectorLayer({source: new VectorSource(),
-    style: function () {
-      return new Style({
-        text: new Text({
-          text: 'person_pin_circle',
-          font: 'normal 30px Material Icons',
-          fill: new Fill({
-            color: 'blue'
-          })
-        })
-      })
-    }});
-  layer.setProperties({lid: geolocationLid, name: 'device gps-position'});
+  layers.remove(layers.getArray().filter(layer => layerId === (layer.get('lid')))[0]);
+  // create new layer
+  let layer = new VectorLayer({
+    source: new VectorSource(),
+    style: geolocationMarker});
+  layer.setProperties({lid: layerId, name: 'actual gps-position'});
   return layer;
 };
 
-function addCoordinatestoLayer (coordinate, layer) {
-  layer.getSource().addFeature(new Feature({geometry: coordinate}));
+function addPointtoLayer (point, layer) {
+  layer.getSource().addFeature(new Feature({geometry: point}));
 };
 
 export default {
   name: 'user-locator',
   data: function () {
     return {
-      isGPSAvailable: null,
-      isPositionFixed: false,
-      isSearchingForPosition: false
+      isGeolocationAPIAvailable: null,
+      isGeolocationFound: false,
+      isSearchingForGeolocation: false
     }
   },
   created () {
@@ -58,27 +60,27 @@ export default {
       me.map = olMap;
     });
     if (!navigator.geolocation) {
-      me.isGPSAvailable = false;
+      me.isGeolocationAPIAvailable = false;
     } else {
-      me.isGPSAvailable = true;
+      me.isGeolocationAPIAvailable = true;
     }
   },
   methods: {
-    locateMePressed () {
+    geolocateUserAndShowMarkerOnMap () {
       var me = this;
-      if (me.isGPSAvailable) {
-        me.isSearchingForPosition = true;
+      if (me.isGeolocationAPIAvailable) {
+        me.isSearchingForGeolocation = true;
         setTimeout(function () {
           navigator.geolocation.getCurrentPosition(position => {
-            var actualGeolocationPoint = new Point(fromLonLat([position.coords.longitude, position.coords.latitude]));
+            var currentPosition = new Point(fromLonLat([position.coords.longitude, position.coords.latitude]));
             if (typeof me.map !== 'undefined') {
-              me.isPositionFixed = true;
-              me.isSearchingForPosition = false;
-              // get a Layer to put the actualGeolocationPoint on
-              let geolocationLayer = getGeolocationLayer(me.map.getLayers());
-              addCoordinatestoLayer(actualGeolocationPoint, geolocationLayer)
+              me.isGeolocationFound = true;
+              me.isSearchingForGeolocation = false;
+              // get a Layer to put the currentPosition on
+              var geolocationLayer = createAndRemoveExistingLayer(me.map.getLayers(), 'userPosition');
+              addPointtoLayer(currentPosition, geolocationLayer);
               me.map.addLayer(geolocationLayer);
-              me.map.getView().fit(actualGeolocationPoint, {minResolution: 3, duration: 3200, padding: [50, 50, 50, 50]});
+              me.map.getView().fit(currentPosition, {minResolution: 3, duration: 3200, padding: [50, 50, 50, 50]});
             } else {
               console.error('the map is not defined in the getCurrentPosition callback');
             }
@@ -88,13 +90,13 @@ export default {
             console.error('Geolocation error : code ' + error.code + ' - ' + error.message);
             // Display user friendly error message
             alert('Geolocation error : code ' + error.code + ' - ' + error.message);
-            me.isGPSAvailable = false;
-            me.isSearchingForPosition = false;
+            me.isGeolocationAPIAvailable = false;
+            me.isSearchingForGeolocation = false;
           },
           {enableHighAccuracy: true, maximumAge: 60000, timeout: 27000})
         }, 1600);
       } else {
-        me.isGPSAvailable = false;
+        me.isGeolocationAPIAvailable = false;
       }
     }
   }
