6a08b518ccae85bab4cc1120c66aaa699b1f8133
Jakob Miksch <jakob@meggsimum.de>
2021-03-24 10:37:06 +0100

    Extend tests

--- test/unit/specs/components/attributeTable/AttributeTable.spec.js
+++ test/unit/specs/components/attributeTable/AttributeTable.spec.js
@@ -1,18 +1,24 @@
+import Vue from 'vue';
 import AttributeTable from '@/components/attributeTable/AttributeTable';
 import { expect } from 'chai';
+import { shallowMount, mount } from '@vue/test-utils';
+
+const appConfig = {modules: { 'wgu-attributetable': {} }};
 
 describe('attributeTable/AttributeTable.vue', () => {
   it('is defined', () => {
     expect(typeof AttributeTable).to.not.equal('undefined');
   });
 
-  it('has a mounted hook', () => {
-    expect(typeof AttributeTable.created).to.equal('function');
+  it('has a created hook', () => {
+    expect(AttributeTable.created).to.be.a('function');
   });
 
   it('sets the correct default data', () => {
-    expect(typeof AttributeTable.data).to.equal('function');
+    expect(AttributeTable.data).to.be.a('function');
     const defaultData = AttributeTable.data();
+    expect(defaultData).to.be.an('object');
+
     expect(defaultData.headers).to.be.an('array');
     expect(defaultData.headers.length).to.eql(0);
 
@@ -24,5 +30,39 @@ describe('attributeTable/AttributeTable.vue', () => {
 
     expect(defaultData.page).to.eql(1);
     expect(defaultData.loading).to.eql(true);
+  });
+
+  describe('props', () => {
+    let comp;
+    beforeEach(() => {
+      Vue.prototype.$appConfig = appConfig;
+      comp = mount(AttributeTable);
+    });
+
+    it('has correct default props', () => {
+      expect(comp.vm.layerId).to.be.null;
+      expect(comp.vm.loadingText).to.not.be.null;
+      expect(comp.vm.forbiddenColumnNames).to.be.an.instanceof(Array);
+    });
+
+    afterEach(() => {
+      comp.destroy();
+      Vue.prototype.$appConfig = undefined;
+    });
+  });
+
+  describe('methods', () => {
+    let comp;
+    let vm;
+    beforeEach(() => {
+      comp = shallowMount(AttributeTable);
+      vm = comp.vm;
+    });
+
+    it('are implemented', () => {
+      expect(vm.populateTable).to.be.a('function');
+      expect(vm.applyRecordsFromOlLayer).to.be.a('function');
+      expect(vm.applyColumnMapping).to.be.a('function');
+    });
   });
 })
--- test/unit/specs/components/attributeTable/AttributeTableWin.spec.js
+++ test/unit/specs/components/attributeTable/AttributeTableWin.spec.js
@@ -1,15 +1,59 @@
+import Vue from 'vue';
 import AttributeTableWin from '@/components/attributeTable/AttributeTableWin'
+import { expect } from 'chai';
+import { mount, shallowMount } from '@vue/test-utils';
+
+const appConfig = {modules: { 'wgu-attributetable-win': {} }};
 
 describe('attributeTable/AttributeTableWin.vue', () => {
   it('is defined', () => {
     expect(typeof AttributeTableWin).to.not.equal('undefined');
   });
 
+  describe('props', () => {
+    let comp;
+    beforeEach(() => {
+      Vue.prototype.$appConfig = appConfig;
+      comp = mount(AttributeTableWin);
+    });
+
+    it('has correct default props', () => {
+      expect(comp.vm.color).to.equal('red darken-3');
+      expect(comp.vm.icon).to.equal('table_chart');
+      expect(comp.vm.title).to.equal('Attribute Table');
+    });
+
+    afterEach(() => {
+      comp.destroy();
+      Vue.prototype.$appConfig = undefined;
+    });
+  });
+
   // Evaluate the results of functions in
   // the raw component options
   it('sets the correct default data', () => {
-    expect(typeof AttributeTableWin.data).to.equal('function');
+    expect(AttributeTableWin.data).to.be.a('function');
     const defaultData = AttributeTableWin.data();
-    expect(typeof defaultData).to.equal('object');
+    expect(defaultData).to.be.an('object');
+
+    expect(defaultData.show).to.be.false;
+    expect(defaultData.layerId).to.be.null;
+    expect(defaultData.layerItems).to.be.null;
+    expect(defaultData.selectedItem).to.be.null;
+  });
+
+  describe('methods', () => {
+    let comp;
+    let vm;
+    beforeEach(() => {
+      comp = shallowMount(AttributeTableWin);
+      vm = comp.vm;
+    });
+
+    it('are implemented', () => {
+      expect(vm.handleLayerSelect).to.be.a('function');
+      expect(vm.onMapBound).to.be.a('function');
+      expect(vm.populateLayerItems).to.be.a('function');
+    });
   });
 });
