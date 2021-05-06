f217980abc85167268cf7af987bf48438e187c62
Felix Schmenger <Felix.Schmenger@geomer.de>
2021-05-06 17:08:03 +0200

    Fix unit tests for WguAppTemplate and add new test cases for sidebar module initialization.

--- test/unit/specs/WguAppTemplate.spec.js
+++ test/unit/specs/WguAppTemplate.spec.js
@@ -25,7 +25,8 @@ describe('WguAppTpl.vue', () => {
 
     it('has correct default data', () => {
       expect(vm.isEmbedded).to.equal(undefined);
-      expect(vm.moduleWins).to.be.an('array');
+      expect(vm.floatingWins).to.be.an('array');
+      expect(vm.sidebarWins).to.be.an('array');
       expect(vm.footerTextLeft).to.equal('footer-left');
       expect(vm.footerTextRight).to.equal('footer-right');
       expect(vm.showCopyrightYear).to.equal(true);
@@ -41,31 +42,52 @@ describe('WguAppTpl.vue', () => {
       vm = comp.vm;
     });
 
-    it('getModuleWinData returns always an array', () => {
+    it('getModuleWinData(\'floating\') returns always an array', () => {
       // mock a window UI instance
-      const moduleData = vm.getModuleWinData();
+      const moduleData = vm.getModuleWinData('floating');
       expect(moduleData).to.be.an('array');
     });
 
-    it('getModuleWinData returns correct data', () => {
+    it('getModuleWinData(\'sidebar\') returns always an array', () => {
+      // mock a window UI instance
+      const moduleData = vm.getModuleWinData('sidebar');
+      expect(moduleData).to.be.an('array');
+    });
+
+    it('getModuleWinData(\'floating\') returns correct data', () => {
       // mock a module conf
       Vue.prototype.$appConfig = {modules: {
         'wgu-infoclick': {
           'target': 'menu',
-          'win': true,
+          'win': 'floating',
           'draggable': false,
           'initPos': {
             'left': 8,
             'top': 16
           }
         }}};
-      const moduleData = vm.getModuleWinData();
+      const moduleData = vm.getModuleWinData('floating');
       expect(moduleData).to.be.an('array');
       expect(moduleData.length).to.equal(1);
       expect(moduleData[0].type).to.equal('wgu-infoclick-win');
+      expect(moduleData[0].target).to.equal('menu');
       expect(moduleData[0].draggable).to.equal(false);
       expect(moduleData[0].initPos.left).to.equal(8);
       expect(moduleData[0].initPos.top).to.equal(16);
+    });
+
+    it('getModuleWinData(\'sidebar\') returns correct data', () => {
+      // mock a module conf
+      Vue.prototype.$appConfig = {modules: {
+        'wgu-infoclick': {
+          'target': 'menu',
+          'win': 'sidebar'
+        }}};
+      const moduleData = vm.getModuleWinData('sidebar');
+      expect(moduleData).to.be.an('array');
+      expect(moduleData.length).to.equal(1);
+      expect(moduleData[0].type).to.equal('wgu-infoclick-win');
+      expect(moduleData[0].target).to.equal('menu');
     });
   });
 });
