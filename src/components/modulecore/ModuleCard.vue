b7d38292fe58e406dfb19ea7be1d382578413f23
Felix Schmenger <Felix.Schmenger@geomer.de>
2021-05-11 14:14:51 +0200

    Default the module card minimizable option to false. This has to be refined for floating windows in conjunction with a better positioning / sizing concept to prevent windows from jumping and resizing.

--- src/components/modulecore/ModuleCard.vue
+++ src/components/modulecore/ModuleCard.vue
@@ -51,7 +51,7 @@
       icon: {type: String, required: true},
       title: {type: String, required: true},
       win: {type: String, required: false, default: 'floating'},
-      minimizable: {type: Boolean, required: false, default: true},
+      minimizable: {type: Boolean, required: false, default: false},
       color: {type: String, required: false, default: 'red darken-3'},
       backgroundImage: {type: String, required: false, default: undefined},
       // Positioning / sizing properties will be ignored for sidebar cards.
