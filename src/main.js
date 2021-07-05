c732cc946a9fcda36341b1a92a884bf266869bb2
Felix Schmenger <Felix.Schmenger@geomer.de>
2021-07-05 17:07:46 +0200

    First draft of importing localization messages. Language files are loaded and merged from 'src/locales' and 'app/locales'.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import PortalVue from 'portal-vue'
+import VueI18n from 'vue-i18n';
 import '@mdi/font/css/materialdesignicons.css'
 import 'material-icons/iconfont/material-icons.css'
 import '../node_modules/ol/ol.css';
@@ -12,6 +13,7 @@ import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
 Vue.use(PortalVue);
+Vue.use(VueI18n);
 
 // necessary for some components
 export default new Vuetify({
@@ -66,13 +68,75 @@ const migrateAppConfig = function (appConfig) {
   return appConfig;
 }
 
+const isObject = function (item) {
+  return (item && typeof item === 'object' && !Array.isArray(item));
+}
+
+/**
+ * Deep in place merge of source object into target object.
+ */
+const mergeDeep = function (target, source) {
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) {
+          Object.assign(target, { [key]: {} });
+        }
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+}
+
+/**
+ * Import webpack context for locale files into target.
+ */
+const importLocales = function (target, context) {
+  const messages = context
+    .keys()
+    .map((key) => ({ key, locale: key.match(/[a-z0-9-_]+/i)[0] }))
+    .reduce(
+      (messages, { key, locale }) => ({
+        ...messages,
+        [locale]: context(key)
+      }),
+      {}
+    );
+  mergeDeep(target, messages);
+}
+
+const createVueI18nContext = function () {
+  let i18nMessages = {};
+
+  importLocales(i18nMessages, require.context('./locales', true, /[a-z0-9-_]+\.json$/i));
+
+  // Try to load an optional app specific language file.
+  try {
+    importLocales(i18nMessages, require.context('../app/locales', true, /[a-z0-9-_]+\.json$/i));
+  } catch (e) {
+  }
+
+  // TODO get locale information from config / browser detection
+  const i18n = new VueI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: i18nMessages
+  });
+
+  return i18n;
+}
+
 const opts = {};
 const createApp = function (appConfig) {
   // make app config accessible for all components
   Vue.prototype.$appConfig = migrateAppConfig(appConfig);
+  const i18nContext = createVueI18nContext(appConfig);
   /* eslint-disable no-new */
   new Vue({
     vuetify: new Vuetify(opts),
+    i18n: i18nContext,
     el: '#app',
     template: '<wgu-app/>',
     components: { WguApp }
