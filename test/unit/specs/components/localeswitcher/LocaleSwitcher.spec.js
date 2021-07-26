f42026d506ec354592284d54f126d43e22350b01
Felix Schmenger <Felix.Schmenger@geomer.de>
2021-07-26 13:12:26 +0200

    Add basic unit tests for new LocaleSwitcher component.

---
+++ test/unit/specs/components/localeswitcher/LocaleSwitcher.spec.js
@@ -0,0 +1,91 @@
+import Vue from 'vue'
+import { mount, shallowMount } from '@vue/test-utils';
+import LocaleSwitcher from '@/components/localeswitcher/LocaleSwitcher';
+
+const appConfig = {
+  'lang': {
+    'supported': {
+      'en': 'English',
+      'de': 'Deutsch'
+    },
+    'fallback': 'de'
+  }
+};
+
+describe('localeswitcher/LocaleSwitcher.vue', () => {
+  it('is defined', () => {
+    expect(LocaleSwitcher).to.not.be.an('undefined');
+  });
+
+  describe('props', () => {
+    let comp;
+    let vm;
+    beforeEach(() => {
+      comp = shallowMount(LocaleSwitcher);
+      vm = comp.vm;
+    });
+
+    it('has correct default props', () => {
+      expect(vm.icon).to.equal('language');
+      expect(vm.dark).to.equal(false);
+    });
+
+    afterEach(() => {
+      comp.destroy();
+    });
+  });
+
+  describe('data', () => {
+    let comp;
+    let vm;
+    beforeEach(() => {
+      Vue.prototype.$appConfig = appConfig;
+      comp = shallowMount(LocaleSwitcher);
+      vm = comp.vm;
+    });
+
+    it('has correct default data', () => {
+      expect(typeof LocaleSwitcher.data).to.equal('function');
+      expect(vm.lang).to.be.an('object');
+      expect(Object.keys(vm.lang).length).to.eql(2);
+      expect(vm.lang['en']).to.eql('English');
+      expect(vm.lang['de']).to.eql('Deutsch');
+    });
+
+    afterEach(() => {
+      comp.destroy();
+    });
+  });
+
+  describe('methods', () => {
+    let comp;
+    let vm;
+    beforeEach(() => {
+      Vue.prototype.$appConfig = appConfig;
+      comp = mount(LocaleSwitcher, {
+        created () {
+          this.$vuetify.lang = { current: 'en' };
+        }
+      });
+      vm = comp.vm;
+    });
+
+    it('are implemented', () => {
+      expect(typeof vm.onItemClick).to.equal('function');
+    });
+
+    it('onItemClick toggles language', () => {
+      vm.onItemClick('de');
+      expect(vm.$i18n.locale).to.equal('de');
+      expect(vm.$vuetify.lang.current).to.equal('de');
+
+      vm.onItemClick('en');
+      expect(vm.$i18n.locale).to.equal('en');
+      expect(vm.$vuetify.lang.current).to.equal('en');
+    });
+
+    afterEach(() => {
+      comp.destroy();
+    });
+  });
+});
