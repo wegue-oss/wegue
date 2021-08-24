348fa4ea61198f849ba4cfee448f80a114d1f46d
Felix Schmenger <Felix.Schmenger@geomer.de>
2021-08-24 15:17:30 +0200

    Update unit tests.

--- test/unit/specs/components/maxextentbutton/ZoomToMaxExtentButton.spec.js
+++ test/unit/specs/components/maxextentbutton/ZoomToMaxExtentButton.spec.js
@@ -2,35 +2,43 @@ import Vue from 'vue'
 import ZoomToMaxExtentButton from '@/components/maxextentbutton/ZoomToMaxExtentButton'
 import OlMap from 'ol/Map';
 import OlView from 'ol/View';
+import { shallowMount } from '@vue/test-utils';
 
 describe('maxextentbutton/ZoomToMaxExtentButton.vue', () => {
-  // Check methods
-  it('has a method onClick', () => {
-    const Constructor = Vue.extend(ZoomToMaxExtentButton);
-    const ztmeb = new Constructor({
-    }).$mount();
-    expect(typeof ztmeb.onClick).to.equal('function');
-  });
-
-  it('onClick sets correct center and zoom', () => {
-    const Constructor = Vue.extend(ZoomToMaxExtentButton);
-    const ztmeb = new Constructor({
-    }).$mount();
-
-    ztmeb.$appConfig = {
+  let comp;
+  let vm;
+  beforeEach(() => {
+    Vue.prototype.$appConfig = {
       mapCenter: [0, 0],
       mapZoom: 0
     };
-    ztmeb.map = new OlMap({
+    comp = shallowMount(ZoomToMaxExtentButton);
+    vm = comp.vm;
+  });
+
+  // Check methods
+  it('has a method onClick', () => {
+    expect(typeof vm.onClick).to.equal('function');
+  });
+
+  it('onClick sets correct center and zoom', () => {
+    vm.map = new OlMap({
       view: new OlView({
         center: [1, 1],
         zoom: 1
       })
     });
 
-    ztmeb.onClick();
-    expect(ztmeb.map.getView().getCenter()[0]).to.equal(0);
-    expect(ztmeb.map.getView().getCenter()[1]).to.equal(0);
-    expect(ztmeb.map.getView().getZoom()).to.equal(0);
+    // Remarks: This works synchronously, if no animation is used.
+    vm.onClick();
+
+    expect(vm.map.getView().getCenter()[0]).to.equal(0);
+    expect(vm.map.getView().getCenter()[1]).to.equal(0);
+    expect(vm.map.getView().getZoom()).to.equal(0);
+  });
+
+  afterEach(() => {
+    comp.destroy();
+    Vue.prototype.$appConfig = undefined;
   });
 });
