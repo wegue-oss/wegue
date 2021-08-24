e2674d95d449341e7c5639286c1fe26162c3fd3f
Felix Schmenger <Felix.Schmenger@geomer.de>
2021-08-24 14:25:12 +0200

    When using fly to a point / coordinate, zoom out to have both the source and target location visible - this is the same behavior as when flying to an extent. The zoomOut option is now implicitly computed and no longer available from configuration.

--- src/util/ViewAnimation.js
+++ src/util/ViewAnimation.js
@@ -212,19 +212,64 @@ const FlyAnimation = {
    * @param {function(complete)} completionCallback An optional notification that the animation has completed.
    * @param {Object} options Configuration object for the animation, supported attributes are:
    * * {Number} duration An optional animation duration.
-   * * {Number} zoomOut An optional zoom out level.
    * * {Number} zoom An optional final zoom level.
    * * {Number} maxZoom An optional maximal zoom level.
    */
   toLocation (view, location, completionCallback, options) {
     // Set defaults if arguments are not provided.
     const duration = options.duration ?? 3000;
-    const zoomOut = options.zoomOut ?? view.getZoom() - 1;
     const zoom = options.zoom ?? view.getZoom();
     const maxZoom = options.maxZoom ?? Infinity;
 
-    // The animation consist of 2 simultaneous parts:
-    // Zoom out then zoom in, while moving to the center location.
+    // Zoom out to have both locations visible.
+    const extentOut = view.calculateExtent();
+    Extent.extend(extentOut, Extent.boundingExtent([location]));
+    const resolutionOut = view.getResolutionForExtent(extentOut);
+    const zoomOut = view.getZoomForResolution(resolutionOut) - 1;
+
+    this.animate(view, location, completionCallback, duration, zoomOut, zoom, maxZoom);
+  },
+
+  /**
+   * Zoom to fit the given extent by using a "fly" animation.
+   * @param {ol.View} view The `ol.View` of the map.
+   * @param {ol.Extent} extent The destination extent to zoom to.
+   * @param {function(complete)} completionCallback An optional notification that the animation has completed.
+   * @param {Object} options Configuration object for the animation, supported attributes are:
+   * * {Number} duration An optional animation duration.
+   * * {Number} maxZoom An optional maximal zoom level.
+   */
+  toExtent (view, extent, completionCallback, options) {
+    // Set defaults if arguments are not provided.
+    const duration = options.duration ?? 3000;
+    const maxZoom = options.maxZoom ?? Infinity;
+
+    // Zoom out to have both locations visible.
+    const extentOut = view.calculateExtent();
+    Extent.extend(extentOut, extent);
+    const resolutionOut = view.getResolutionForExtent(extentOut);
+    const zoomOut = view.getZoomForResolution(resolutionOut) - 1;
+
+    // Then zoom in to the given extent.
+    const resolutionIn = view.getResolutionForExtent(extent);
+    const zoom = view.getZoomForResolution(resolutionIn) - 0.2;
+
+    const location = Extent.getCenter(extent);
+    this.animate(view, location, completionCallback, duration, zoomOut, zoom, maxZoom);
+  },
+
+  /**
+   * The animation consist of 2 simultaneous parts:
+   * Zoom out then zoom in, while moving to the center location.
+   * @param {ol.View} view The `ol.View` of the map.
+   * @param {ol.Coordinate} location The destination center point to zoom to.
+   * @param {function(complete)} completionCallback An optional notification that the animation has completed.
+   * @param {Number} duration The animation duration.
+   * @param {Number} zoomOut The zoom out level.
+   * @param {Number} zoom The final zoom level.
+   * @param {Number} maxZoom The maximal zoom level.
+   */
+  animate (view, location, completionCallback, duration, zoomOut, zoom, maxZoom) {
     var parts = 2;
     var finished = false;
 
@@ -253,39 +298,6 @@ const FlyAnimation = {
       zoom: Math.min(zoom, maxZoom),
       duration: duration / 2
     }, callback);
-  },
-
-  /**
-   * Zoom to fit the given extent by using a "fly" animation.
-   * @param {ol.View} view The `ol.View` of the map.
-   * @param {ol.Extent} extent The destination extent to zoom to.
-   * @param {function(complete)} completionCallback An optional notification that the animation has completed.
-   * @param {Object} options Configuration object for the animation, supported attributes are:
-   * * {Number} duration An optional animation duration.
-   * * {Number} maxZoom An optional maximal zoom level.
-   */
-  toExtent (view, extent, completionCallback, options) {
-    // Set defaults if arguments are not provided.
-    const duration = options.duration ?? 3000;
-    const maxZoom = options.maxZoom ?? Infinity;
-
-    // Zoom out to have both locations visible
-    const extentOut = view.calculateExtent();
-    Extent.extend(extentOut, extent);
-    const resolutionOut = view.getResolutionForExtent(extentOut);
-    const zoomOut = view.getZoomForResolution(resolutionOut) - 1;
-
-    // Then zoom in to the given extent.
-    const resolutionIn = view.getResolutionForExtent(extent);
-    const zoom = view.getZoomForResolution(resolutionIn) - 0.2;
-
-    const location = Extent.getCenter(extent);
-    this.toLocation(view, location, completionCallback, {
-      duration: duration,
-      zoomOut: zoomOut,
-      zoom: zoom,
-      maxZoom: maxZoom
-    });
   }
 };
 
