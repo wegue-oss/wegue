b7572496ab39ef2a421241cc27e4ab74a77c7553
Felix Schmenger <Felix.Schmenger@geomer.de>
2021-12-07 09:38:14 +0100

    First implementation of WMS GetFeatureInfo requests.

--- src/components/ol/HoverController.js
+++ src/components/ol/HoverController.js
@@ -1,4 +1,9 @@
 import Overlay from 'ol/Overlay';
+import TileWmsSource from 'ol/source/TileWMS';
+import ImageWMSSource from 'ol/source/ImageWMS';
+import VectorSource from 'ol/source/Vector';
+import WMSGetFeatureInfo from 'ol/format/WMSGetFeatureInfo';
+import axios from 'axios';
 
 export default class HoverController {
   map = null;
@@ -51,14 +56,90 @@ export default class HoverController {
   onPointerMove (event) {
     const me = this;
     const map = me.map;
-    const overlayEl = me.overlayEl;
+    const pixel = event.pixel;
     let hoverAttr;
-    const features = map.getFeaturesAtPixel(event.pixel, { layerFilter: (layer) => {
-      if (layer.get('hoverable')) {
-        hoverAttr = layer.get('hoverAttribute');
+
+    map.forEachLayerAtPixel(pixel, (layer, pixelValues) => {
+      if (!layer.get('hoverable')) {
+        return;
       }
-      return layer.get('hoverable');
-    } });
+      var source = layer.getSource();
+      if (source instanceof TileWmsSource || source instanceof ImageWMSSource) {
+        me.getWMSFeaturesAsync(map, layer, event.coordinate)
+          .then(function (features) {
+            hoverAttr = layer.get('hoverAttribute');
+            me.displayTooltip(features, hoverAttr, event.coordinate)
+          })
+          .catch(function (error) {
+            console.error(error);
+          })
+      } else if (source instanceof VectorSource) {
+        hoverAttr = layer.get('hoverAttribute');
+        const features = me.getVectorFeatures(map, layer, pixel);
+        me.displayTooltip(features, hoverAttr, event.coordinate)
+      }
+    });
+  }
+
+  /**
+   *
+   * @param {*} map
+   * @param {*} layer
+   * @param {*} pixel
+   * @returns
+   */
+  getVectorFeatures (map, layer, pixel) {
+    const features = map.getFeaturesAtPixel(pixel, {
+      layerFilter: (layerCand) => {
+        return layerCand === layer;
+      }
+    });
+    return features;
+  }
+
+  /**
+   *
+   * @param {*} map
+   * @param {*} layer
+   * @param {*} coordinate
+   * @returns
+   */
+  getWMSFeaturesAsync (map, layer, coordinate) {
+    const view = map.getView();
+    return new Promise((resolve, reject) => {
+      const url = layer.getSource().getFeatureInfoUrl(
+        coordinate,
+        view.getResolution(),
+        view.getProjection(),
+        {
+          'INFO_FORMAT': 'application/vnd.ogc.gml/3.1.1'
+        }
+      );
+
+      if (!url) {
+        reject(new Error('URL is undefined'));
+      }
+
+      const request = {
+        method: 'GET',
+        url: url
+      };
+
+      axios(request)
+        .then(response => {
+          const features = new WMSGetFeatureInfo().readFeatures(response.data);
+          resolve(features);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  }
+
+  displayTooltip (features, hoverAttr, coordinate) {
+    const me = this;
+    const overlayEl = me.overlayEl;
+
     if (!features || features.length === 0 || !hoverAttr) {
       hoverAttr = null;
       overlayEl.innerHTML = null;
@@ -68,6 +149,6 @@ export default class HoverController {
     const feature = features[0];
     var attr = feature.get(hoverAttr);
     overlayEl.innerHTML = attr;
-    me.overlay.setPosition(event.coordinate);
+    me.overlay.setPosition(coordinate);
   }
-};
+}
