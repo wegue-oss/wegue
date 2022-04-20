4619cf9b3ffad4100703bfaa8344259a7c4bff68
sronveaux <s.ronveaux@biodiversity.be>
2022-04-20 11:21:30 +0200

    Changed eslint-html-plugin to eslint-vue-plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,14 +9,19 @@ module.exports = {
   env: {
     browser: true,
   },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential', 
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ],
   // required to lint *.vue files
   plugins: [
-    'html'
+    'vue'
   ],
   // add your custom rules here
-  'rules': {
+  rules: {
     // allow paren-less arrow functions
     'arrow-parens': 'off',
     // allow async-await
