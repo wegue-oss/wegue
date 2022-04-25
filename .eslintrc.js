91c317423c942c418738dd417a94ec0bc6dc5ca5
sronveaux <s.ronveaux@biodiversity.be>
2022-04-25 22:00:43 +0200

    Made changes to conform to new linting configuration format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,17 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
-  parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module'
-  },
   env: {
-    browser: true,
+    node: true,
   },
   extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    'plugin:vue/essential',
+    '@vue/standard'
   ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
   rules: {
-    // allow paren-less arrow functions
-    'arrow-parens': 'off',
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
     // allow semicolons and the end of a statement
     'semi': 'off',
 
@@ -45,5 +29,18 @@ module.exports = {
     'padded-blocks': process.env.NODE_ENV  === 'development' ? 'warn' : 'error',
     'keyword-spacing': process.env.NODE_ENV  === 'development' ? 'warn' : 'error',
     'arrow-spacing': process.env.NODE_ENV  === 'development' ? 'warn' : 'error'
-  }
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)'
+      ],
+      env: {
+        mocha: true
+      }
+    }
+  ]
 }
