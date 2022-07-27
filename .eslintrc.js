aa02309d0b838a4ad3c4adaf42ab09a0fe15452c
sronveaux <s.ronveaux@biodiversity.be>
2022-07-27 16:09:09 +0200

    Changed .browserlistrc .gitignore and .eslinrc.js to conform to new Vue template

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,8 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
-    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 
     // allow semicolons and the end of a statement
     'semi': 'off',
