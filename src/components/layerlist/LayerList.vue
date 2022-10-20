130d6ec8b421315db26aaf54ccfbc3a73d6f35bf
Felix Schmenger <Felix.Schmenger@geomer.de>
2022-10-20 17:32:28 +0200

    Factor out layer list items into a separate template.

--- src/components/layerlist/LayerList.vue
+++ src/components/layerlist/LayerList.vue
@@ -1,70 +1,24 @@
 <template>
 
   <v-list expand>
-    <template v-for="layer in displayedLayers">
-
-      <!-- Show layer details -->
-      <v-list-group
-          v-if="showDetails(layer)"
-          :value="false"
-          :key="layer.lid"
-          class="text--primary"         
-          >
-          <template v-slot:activator>
-            <v-list-item-action>
-              <v-checkbox
-                color="secondary"
-                hide-details
-                :input-value="layer.getVisible()"
-                @click.capture.stop="onItemClick(layer)"   
-              />
-            </v-list-item-action> 
-            <v-list-item-title>
-              {{ layer.get('name') }}
-            </v-list-item-title>
-          </template>
-          
-          <v-list-item>
-            <wgu-layerlegendimage
-              :layer="layer"
-              :mapView="map.getView()"
-            />
-          </v-list-item>
-        </v-list-group>
-
-        <!-- Simple layer entry -->
-        <v-list-item 
-           v-if="!showDetails(layer)"
-           class="wgu-layerlist-item" 
-          :key="layer.lid" 
-          >
-          <v-list-item-action>
-            <v-checkbox
-              color="secondary"
-              hide-details
-              :input-value="layer.getVisible()"
-               @click.capture.stop="onItemClick(layer)"
-            />
-          </v-list-item-action>
-          <v-list-item-content>
-              <v-list-item-title>
-                {{ layer.get('name') }}
-              </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-    </template>
+    <wgu-layerlistitem
+      v-for="layer in displayedLayers"
+      :key="layer.lid"
+      :layer="layer"
+      :mapView="map.getView()"
+      :showDetails="showDetails(layer)"
+    />
   </v-list> 
 </template>
 
 <script>
   import { Mapable } from '../../mixins/Mapable';
-  import LayerLegendImage from './LayerLegendImage'
+  import LayerListItem from './LayerListItem'
   
   export default {
     name: 'wgu-layerlist',
     components: {
-      'wgu-layerlegendimage': LayerLegendImage
+      'wgu-layerlistitem': LayerListItem
     },
     mixins: [Mapable],
     props: {
@@ -84,19 +38,10 @@
         this.layers = this.map.getLayers().getArray();
       },
       /**
-       * Handler for click on item in layer list:
-       * Toggles the corresponding layer visibility.
-       *
-       * @param  {Object} layer Layer object
-       */
-      onItemClick (layer) {
-        layer.setVisible(!layer.getVisible());
-      },
-      /**
        * Returns true, if the layer item should show an extension slider with layer details.
        **/
       showDetails (layer) {
-        return this.showLegends && layer.get('legend');
+        return this.showLegends && !!layer.get('legend');
       }
     },
     computed: {
---
+++ src/components/layerlist/LayerListItem.vue
@@ -0,0 +1,73 @@
+<template>
+  <!-- Show layer details -->
+  <v-list-group
+    v-if="showDetails"
+    :value="false"
+    class="text--primary"         
+    >
+    <template v-slot:activator>
+      <v-list-item-action>
+        <v-checkbox
+          color="secondary"
+          hide-details
+          :input-value="layer.getVisible()"
+          @click.capture.stop="onItemClick()"   
+        />
+      </v-list-item-action> 
+      <v-list-item-title>
+        {{ layer.get('name') }}
+      </v-list-item-title>
+    </template>
+    
+    <v-list-item>
+      <wgu-layerlegendimage
+        :layer="layer"
+        :mapView="mapView"
+      />
+    </v-list-item>
+  </v-list-group>
+
+  <!-- Simple layer entry -->
+  <v-list-item 
+    v-else
+    class="wgu-layerlist-item" 
+    >
+    <v-list-item-action>
+      <v-checkbox
+        color="secondary"
+        hide-details
+        :input-value="layer.getVisible()"
+        @click.capture.stop="onItemClick(layer)"
+      />
+    </v-list-item-action>
+    <v-list-item-content>
+      <v-list-item-title>
+        {{ layer.get('name') }}
+      </v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+import LayerLegendImage from './LayerLegendImage'
+
+export default {
+  name: 'wgu-layerlistitem',
+  components: {
+    'wgu-layerlegendimage': LayerLegendImage
+  },
+  props: {
+    layer: { type: Object, required: true },
+    mapView: { type: Object, required: true },
+    showDetails: { type: Boolean, required: true }
+  },
+  methods: {
+    /**
+     * Handler for click on layer item, toggles the layer`s visibility.
+     */
+    onItemClick () {
+      this.layer.setVisible(!this.layer.getVisible());
+    }
+  }
+};
+</script>
