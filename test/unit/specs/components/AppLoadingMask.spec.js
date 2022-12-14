10d08755353f409f2af0bfe7a06c7a3bf9822384
Christian Mayer <chris@meggsimum.de>
2022-12-14 10:35:05 +0100

    Add more unit tests for AppLoadingMask

--- test/unit/specs/components/AppLoadingMask.spec.js
+++ test/unit/specs/components/AppLoadingMask.spec.js
@@ -1,4 +1,6 @@
-import AppLoadingMask from '@/components/AppLoadingMask'
+import AppLoadingMask from '@/components/AppLoadingMask';
+import { WguEventBus } from '@/WguEventBus';
+import { shallowMount } from '@vue/test-utils';
 
 describe('AppLoadingMask.vue', () => {
   it('sets the correct default data', () => {
@@ -6,5 +8,29 @@ describe('AppLoadingMask.vue', () => {
     expect(typeof AppLoadingMask.data).to.equal('function');
     const data = AppLoadingMask.data();
     expect(data.show).to.equal(false);
+  });
+
+  describe('events', () => {
+    let comp;
+    let vm;
+    beforeEach(() => {
+      comp = shallowMount(AppLoadingMask, {});
+      vm = comp.vm;
+    });
+
+    it('event "app-loading-mask-toggle" forces correct visibility', done => {
+      // force showing by adding 'true' parameter
+      WguEventBus.$emit('app-loading-mask-toggle', true);
+      vm.$nextTick(() => {
+        expect(vm.show).to.equal(true);
+
+        // toggle visibility by skipping parameter
+        WguEventBus.$emit('app-loading-mask-toggle');
+        vm.$nextTick(() => {
+          expect(vm.show).to.equal(false);
+          done();
+        });
+      });
+    });
   });
 });
