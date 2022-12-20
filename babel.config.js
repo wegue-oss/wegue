d6448a788408d3aa4e2711ed417de6745ade5908
sronveaux <s.ronveaux@biodiversity.be>
2022-12-20 13:12:21 +0100

    Removed two Babel polyfills which are now included in core-js@3

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,6 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  plugins: [
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-optional-chaining'
-  ],
   env: {
     test: {
       plugins: ['istanbul']
