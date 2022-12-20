5d1cb1b9984a958e761e55a1d6038010656ee21f
sronveaux <s.ronveaux@biodiversity.be>
2022-12-20 12:59:03 +0100

    Fixed some linter errors in app-starter linked to the new rules of eslint6

--- app-starter/WguAppTemplate.vue
+++ app-starter/WguAppTemplate.vue
@@ -68,94 +68,94 @@
 </template>
 
 <script>
-  import Vue from 'vue'
-  import { WguEventBus } from '../src/WguEventBus'
-  import OlMap from '../src/components/ol/Map'
-  import HoverTooltip from '../src/components/ol/HoverTooltip'
-  import AppHeader from './components/AppHeader'
-  import AppFooter from './components/AppFooter'
-  import AppSidebar from './components/AppSidebar'
-  import AppLogo from '../src/components/AppLogo'
-  import AppLoadingMask from '../src/components/AppLoadingMask'
-  import BgLayerSwitcher from '../src/components/bglayerswitcher/BgLayerSwitcher.vue'
-  import OverviewMap from '../src/components/overviewmap/OverviewMap.vue'
-  import MeasureWin from '../src/components/measuretool/MeasureWin'
-  import LayerListWin from '../src/components/layerlist/LayerListWin'
-  import HelpWin from '../src/components/helpwin/HelpWin'
-  import InfoClickWin from '../src/components/infoclick/InfoClickWin'
-  import MapLoadingStatus from '../src/components/progress/MapLoadingStatus'
-  import AttributeTableWin from '../src/components/attributeTable/AttributeTableWin.vue'
-  import MapRecorderWin from '../src/components/maprecorder/MapRecorderWin'
-  import SampleModuleWin from './components/SampleModule.vue'
+import Vue from 'vue'
+import { WguEventBus } from '../src/WguEventBus'
+import OlMap from '../src/components/ol/Map'
+import HoverTooltip from '../src/components/ol/HoverTooltip'
+import AppHeader from './components/AppHeader'
+import AppFooter from './components/AppFooter'
+import AppSidebar from './components/AppSidebar'
+import AppLogo from '../src/components/AppLogo'
+import AppLoadingMask from '../src/components/AppLoadingMask'
+import BgLayerSwitcher from '../src/components/bglayerswitcher/BgLayerSwitcher.vue'
+import OverviewMap from '../src/components/overviewmap/OverviewMap.vue'
+import MeasureWin from '../src/components/measuretool/MeasureWin'
+import LayerListWin from '../src/components/layerlist/LayerListWin'
+import HelpWin from '../src/components/helpwin/HelpWin'
+import InfoClickWin from '../src/components/infoclick/InfoClickWin'
+import MapLoadingStatus from '../src/components/progress/MapLoadingStatus'
+import AttributeTableWin from '../src/components/attributeTable/AttributeTableWin.vue'
+import MapRecorderWin from '../src/components/maprecorder/MapRecorderWin'
+import SampleModuleWin from './components/SampleModule.vue'
 
-  export default {
-    name: 'wgu-app-tpl',
-    components: {
-      'wgu-map': OlMap,
-      'wgu-hover-tooltip': HoverTooltip,
-      'wgu-app-header': AppHeader,
-      'wgu-app-footer': AppFooter,
-      'wgu-app-sidebar': AppSidebar,
-      'wgu-app-logo': AppLogo,
-      'wgu-app-loading-mask': AppLoadingMask,
-      'wgu-bglayerswitcher': BgLayerSwitcher,
-      'wgu-overviewmap': OverviewMap,
-      'wgu-measuretool-win': MeasureWin,
-      'wgu-layerlist-win': LayerListWin,
-      'wgu-helpwin-win': HelpWin,
-      'wgu-infoclick-win': InfoClickWin,
-      'wgu-maploading-status': MapLoadingStatus,
-      'wgu-attributetable-win': AttributeTableWin,
-      'wgu-maprecorder-win': MapRecorderWin,
-      'sample-module-win': SampleModuleWin
-    },
-    data () {
-      return {
-        isEmbedded: false,
-        sidebarConfig: this.getSidebarConfig(),
-        overviewMapConfig: this.getOverviewMapConfig(),
-        floatingWins: this.getModuleWinData('floating'),
-        sidebarWins: this.getModuleWinData('sidebar'),
-        showCopyrightYear: Vue.prototype.$appConfig.showCopyrightYear
-      }
-    },
-    created () {
-      this.setDocumentTitle();
-    },
-    mounted () {
-      // apply the isEmbedded state to the member var
-      this.isEmbedded = this.$isEmbedded;
+export default {
+  name: 'wgu-app-tpl',
+  components: {
+    'wgu-map': OlMap,
+    'wgu-hover-tooltip': HoverTooltip,
+    'wgu-app-header': AppHeader,
+    'wgu-app-footer': AppFooter,
+    'wgu-app-sidebar': AppSidebar,
+    'wgu-app-logo': AppLogo,
+    'wgu-app-loading-mask': AppLoadingMask,
+    'wgu-bglayerswitcher': BgLayerSwitcher,
+    'wgu-overviewmap': OverviewMap,
+    'wgu-measuretool-win': MeasureWin,
+    'wgu-layerlist-win': LayerListWin,
+    'wgu-helpwin-win': HelpWin,
+    'wgu-infoclick-win': InfoClickWin,
+    'wgu-maploading-status': MapLoadingStatus,
+    'wgu-attributetable-win': AttributeTableWin,
+    'wgu-maprecorder-win': MapRecorderWin,
+    'sample-module-win': SampleModuleWin
+  },
+  data () {
+    return {
+      isEmbedded: false,
+      sidebarConfig: this.getSidebarConfig(),
+      overviewMapConfig: this.getOverviewMapConfig(),
+      floatingWins: this.getModuleWinData('floating'),
+      sidebarWins: this.getModuleWinData('sidebar'),
+      showCopyrightYear: Vue.prototype.$appConfig.showCopyrightYear
+    }
+  },
+  created () {
+    this.setDocumentTitle();
+  },
+  mounted () {
+    // apply the isEmbedded state to the member var
+    this.isEmbedded = this.$isEmbedded;
 
-      // make the refs (floating module window, which are not connected to their
-      // related components, e.g. buttons to toggle them)
-      const refs = this.$refs;
-      let cmpLookup = {};
-      for (const key of Object.keys(refs)) {
-        cmpLookup[key] = refs[key][0];
-      }
-      Vue.prototype.cmpLookup = cmpLookup;
-      // inform registered cmps that the app is mounted and the dynamic
-      // components are available
-      WguEventBus.$emit('app-mounted');
-    },
-    methods: {
-      /**
+    // make the refs (floating module window, which are not connected to their
+    // related components, e.g. buttons to toggle them)
+    const refs = this.$refs;
+    const cmpLookup = {};
+    for (const key of Object.keys(refs)) {
+      cmpLookup[key] = refs[key][0];
+    }
+    Vue.prototype.cmpLookup = cmpLookup;
+    // inform registered cmps that the app is mounted and the dynamic
+    // components are available
+    WguEventBus.$emit('app-mounted');
+  },
+  methods: {
+    /**
        * Returns the configuration object for the sidebar from app-config.
        * @return {Object} Sidebar configuration object.
        */
-      getSidebarConfig () {
-        const appConfig = Vue.prototype.$appConfig || {};
-        return appConfig['sidebar'];
-      },
-      /**
+    getSidebarConfig () {
+      const appConfig = Vue.prototype.$appConfig || {};
+      return appConfig.sidebar;
+    },
+    /**
        * Returns the configuration object for the overview map from app-config.
        * @return {Object} Overview map configuration object.
        */
-      getOverviewMapConfig () {
-        const appConfig = Vue.prototype.$appConfig || {};
-        return appConfig['overviewMap'];
-      },
-      /**
+    getOverviewMapConfig () {
+      const appConfig = Vue.prototype.$appConfig || {};
+      return appConfig.overviewMap;
+    },
+    /**
        * Determines the module window configuration objects from app-config:
        *     moduleWins: [
        *       {type: 'wgu-layerlist-win'},
@@ -164,35 +164,35 @@
        * @param  {String} target Either 'floating' or 'sidebar'
        * @return {Array} module window configuration objects
        */
-      getModuleWinData (target) {
-        const appConfig = Vue.prototype.$appConfig || {};
-        const modulesConfs = appConfig.modules || {};
-        let moduleWins = [];
-        for (const key of Object.keys(modulesConfs)) {
-          const moduleOpts = appConfig.modules[key];
-          if (moduleOpts.win === target) {
-            moduleWins.push({
-              type: key + '-win',
-              ...moduleOpts
-            });
-          }
+    getModuleWinData (target) {
+      const appConfig = Vue.prototype.$appConfig || {};
+      const modulesConfs = appConfig.modules || {};
+      const moduleWins = [];
+      for (const key of Object.keys(modulesConfs)) {
+        const moduleOpts = appConfig.modules[key];
+        if (moduleOpts.win === target) {
+          moduleWins.push({
+            type: key + '-win',
+            ...moduleOpts
+          });
         }
-        return moduleWins;
-      },
-      /**
+      }
+      return moduleWins;
+    },
+    /**
        * Sets the document title from language file.
        */
-      setDocumentTitle () {
-        document.title = this.$t('app.browserTitle') || document.title;
-      }
-    },
-    watch: {
-      /**
+    setDocumentTitle () {
+      document.title = this.$t('app.browserTitle') || document.title;
+    }
+  },
+  watch: {
+    /**
        * Watch for locale changes.
        */
-      '$i18n.locale': function () {
-        this.setDocumentTitle();
-      }
+    '$i18n.locale': function () {
+      this.setDocumentTitle();
     }
   }
+}
 </script>
