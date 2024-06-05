20b50d7ef526c6eaeb1e8fde91fac1ad97bb2f4e
sronveaux <s.ronveaux@biodiversity.be>
2024-06-05 16:58:48 +0200

    Changed WguEventBus code to use tiny-emitter and added  function

--- src/WguEventBus.js
+++ src/WguEventBus.js
@@ -1,9 +1,13 @@
-import mitt from 'mitt'
+import Emitter from 'tiny-emitter';
 
-const emitter = mitt()
+const emitter = new Emitter();
+// import mitt from 'mitt'
+
+// const emitter = mitt()
 
 const WguEventBus = {
   $on: (...args) => emitter.on(...args),
+  $once: (...args) => emitter.once(...args),
   $off: (...args) => emitter.off(...args),
   $emit: (...args) => emitter.emit(...args)
 }
