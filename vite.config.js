77043e89c99343451e542bb9a4fa2f0695dd5c5f
sronveaux <s.ronveaux@biodiversity.be>
2025-09-05 15:00:48 +0200

    Added first simple version of Vite config file

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          compatConfig: {
+            MODE: 3,
+          },
+        },
+      },
+    }),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'APP': fileURLToPath(new URL('./app', import.meta.url)),
+      vue: '@vue/compat'
+    },
+  }
+})
