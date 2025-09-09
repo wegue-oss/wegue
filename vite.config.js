4c1ebc5b0a9f552155c105bb355faddf6ac45636
sronveaux <s.ronveaux@biodiversity.be>
2025-09-09 11:26:00 +0200

    Added missing Vue feature flags in Vite config file

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,11 @@ import Vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+      __VUE_OPTIONS_API__: 'true',
+      __VUE_PROD_DEVTOOLS__: 'false',
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+  },
   plugins: [
     vue({
       template: {
