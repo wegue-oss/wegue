81810e1c6ae0cdc89bd348327a18121c02efca16
sronveaux <s.ronveaux@biodiversity.be>
2025-10-02 09:36:20 +0200

    Adapted Karma config file and source import script to work properly with Vite

--- tests/unit/karma.conf.cjs
+++ tests/unit/karma.conf.cjs
@@ -1,21 +1,25 @@
 // This is a karma config file. For more details see
 //   http://karma-runner.github.io/0.13/config/configuration-file.html
-// we are also using it with karma-webpack
-//   https://github.com/webpack/karma-webpack
+// we are also using it with karma-vite
+//   https://github.com/credred/karma-vite
 
-const webpackConfig = require('../../node_modules/@vue/cli-service/webpack.config.js');
+const path = require('path');
+const projectRoot = path.resolve(__dirname, '../../');
 
 // Optionally override the specfile index.js
 // Idea from: https://glebbahmutov.com/blog/debugging-karma-unit-tests/
-let specFile = './index.js';
-const preprocessors = ['webpack', 'sourcemap'];
-let preprocessorFiles = { './index.js': preprocessors };
+let specFile = 'index.js';
+const fileList = [{
+  pattern: path.resolve(__dirname, specFile),
+  type: 'module',
+  watched: false,
+  served: false
+}];
 
 const argSpecFileIndex = process.argv.indexOf('--spec-file');
 if (argSpecFileIndex > -1) {
   specFile = process.argv[argSpecFileIndex + 1];
-  preprocessorFiles = {};
-  preprocessorFiles[specFile] = preprocessors;
+  fileList[0].pattern = path.resolve(__dirname, specFile);
 }
 
 module.exports = function (config) {
@@ -25,16 +29,25 @@ module.exports = function (config) {
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
     browsers: ['ChromeHeadless'],
-    frameworks: ['mocha', 'sinon-chai', 'webpack'],
+    frameworks: ['mocha', 'sinon-chai', 'vite'],
     reporters: ['spec', 'coverage'],
-    files: [specFile],
-    preprocessors: preprocessorFiles,
-    webpack: webpackConfig,
-    webpackMiddleware: {
-      noInfo: true
+    basePath: projectRoot,
+    files: fileList,
+    preprocessors: {},
+    client: {
+      mocha: {
+        reporter: 'html'
+      }
+    },
+    vite: {
+      root: projectRoot,
+      logLevel: 'debug',
+      server: {
+        middlewareMode: true
+      }
     },
     coverageReporter: {
-      dir: './coverage',
+      dir: './tests/unit/coverage',
       reporters: [
         { type: 'lcov', subdir: '.' },
         { type: 'text-summary' }
@@ -44,4 +57,4 @@ module.exports = function (config) {
       level: 'log'
     }
   })
-}
+};
